9190694d18b8ff20659b381f3e43677dd0c79e83
Oscar Lopera <oscar.lopera@sofka.com.co>
2021-03-29 19:03:22 -0500

    Proyecto Base

---
+++ src/test/java/com/sofka/ejemploCrud/EjemploCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.ejemploCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
